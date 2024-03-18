7824ad71e75b72500c05720f7af3fc26615750d6
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-03-18 01:52:01 +0000

    First commit - vim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,41 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- changes x behaviour to actually delete, no longer has cut behaviour
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+--- lets you cp stuff elsewhere from vim
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+--- Sets ctrl+c same as esc (needed for visblock edit)
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+--- visual block cp
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+
+--- Q not much use
+vim.keymap.set("n", "Q", "<nop>")
+
+--- Switch files fast
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+--- Replace highlighted words
+--- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+--- Changes bash file to exec
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
