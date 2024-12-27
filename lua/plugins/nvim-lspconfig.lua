95baf92a9f6926840bd67932864c00221811100c
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-12-27 19:38:05 +0000

    Fix broken nvim-cmp by switching to blink-cmp
    New Lazynvim version no longer uses nvim-cmp as the completion plugin.

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,43 @@
+--  By default, Neovim supports a subset of the LSP specification.
+--  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+-- LSP servers and clients communicate which features they support through "capabilities".
+--  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+--
+-- This can vary by config, but in general for nvim-lspconfig:
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "saghen/blink.cmp",
+    },
+
+    -- example using `opts` for defining servers
+    -- :help lspconfig-all
+    opts = {
+      servers = {
+        clangd = {},
+        cmake = {},
+        cssls = {},
+        html = {},
+        lua_ls = {},
+        pyright = {},
+        ruff = {},
+      },
+    },
+
+    config = function(_, opts)
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local lspconfig = require("lspconfig")
+      require("lspconfig").lua_ls.setup({ capabilities = capabilities })
+
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end,
+  },
+}
