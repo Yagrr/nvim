20c9bd6aee814388cae2a1d4920b80d45c046e84
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-12-28 15:05:35 +0000

    Add image.lua and molten.lua to their own python workflow folder and disabled them

--- lua/plugins/py-workflow/molten.lua
+++ lua/plugins/py-workflow/molten.lua
@@ -1,6 +1,7 @@
 return {
   {
     "benlubas/molten-nvim",
+    enabled = false,
     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     dependencies = { "3rd/image.nvim" },
     build = ":UpdateRemotePlugins",
@@ -8,28 +9,21 @@ return {
       -- these are examples, not defaults. Please see the readme
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
-    end,
-    config = function()
-      vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
-      vim.keymap.set(
-        "n",
-        "<leader>me",
-        ":MoltenEvaluateOperator<CR>",
-        { silent = true, desc = "run operator selection" }
-      )
-      vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
-      vim.keymap.set(
-        "n",
-        "<leader>mrr",
-        ":Molteernel will get a kernel_id which is most commonly just the name of the kernel. If you try to initialize twonReevaluateCell<CR>",
-        { silent = true, desc = "re-evaluate cell" }
-      )
-      vim.keymap.set(
-        "v",
-        "<leader>r",
-        ":<C-u>MoltenEvaluateVisual<CR>gv",
-        { silent = true, desc = "evaluate visual selection" }
-      )
+
+      --vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
+      --vim.keymap.set(
+      --  "n",
+      --  "<localleader>me",
+      --  ":MoltenEvaluateOperator<CR>",
+      --  { silent = true, desc = "run operator selection" }
+      --)
+      --vim.keymap.set("n", "<localleader>ll", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
+      --vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
+      --vim.keymap.set(
+      --  "v",
+      --  "<localleader>mr",
+      --  ":<C-u>MoltenEvaluateVisual<CR>gv",
+      --  { silent = true, desc = "evaluate visual selection" })
     end,
   },
   {
