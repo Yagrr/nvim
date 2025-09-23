1c6c146597384e4f977d8af122a4cf8664908527
Yagrr <90156764+Yagrr@users.noreply.github.com>
2025-09-23 21:25:12 +0100

    update nvim - removed autocmd due to bug

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,13 +3,13 @@
 -- Add any additional autocmds here
 
 -- Fix folding
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  callback = function()
-    if require("nvim-treesitter.parsers").has_parser() then
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    else
-      vim.opt.foldmethod = "syntax"
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   callback = function()
+--     if require("nvim-treesitter.parsers").has_parser() then
+--       vim.opt.foldmethod = "expr"
+--       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+--     else
+--       vim.opt.foldmethod = "syntax"
+--     end
+--   end,
+-- })
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,8 +8,8 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
       "saghen/blink.cmp",
     },
 
